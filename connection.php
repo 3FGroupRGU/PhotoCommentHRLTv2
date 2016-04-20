fca613a914553424eacb8c9fd86ca7745b2822ea
HenryRL <1300427@rgu.ac.uk>
2016-04-20 15:44:13 +0100

    connection update 15:43 200416

--- connection.php
+++ connection.php
@@ -1,8 +1,8 @@
 <?php
-define('DB_SERVER', 'eu-cdbr-azure-north-d.cloudapp.net');
-define('DB_USERNAME', 'b99256e599d9fc');
-define('DB_PASSWORD', '64c23a04');
-define('DB_DATABASE', 'rgutest');
+define('DB_SERVER', 'eu-cdbr-azure-north-e.cloudapp.net');
+define('DB_USERNAME', 'b007c3ffbd1a09');
+define('DB_PASSWORD', 'e4d0b324');
+define('DB_DATABASE', 'rgutest_cmm503_HRLT');
 $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 
 ?>
