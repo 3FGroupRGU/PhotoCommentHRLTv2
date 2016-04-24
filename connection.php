3869b7e3b77630dae2cc02791e18debe2707ca5a
HenryRL <1300427@rgu.ac.uk>
2016-04-24 18:31:28 +0100

    connection file prepares statement 18:31 240416

--- connection.php
+++ connection.php
@@ -6,26 +6,26 @@
 //$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 
 //prepared procedure 'Login1'
-$mysqli = new mysqli("eu-cdbr-azure-north-e.cloudapp.net","b007c3ffbd1a09", "e4d0b324", "rgutest_cmm503_HRLT");
-if($mysqli->connect_errno) {
-    echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ")" . $mysqli->connect_error;
-}{
-    if(!$mysqli->query("DROP PROCEDURE IF EXISTS Login1") ||
-        !$mysqli->query("CREATE PROCEDURE Login1(IN id_val INT) BEGIN INSERT INTO test(id) VALUES(id_val); END;"))
+//$mysqli = new mysqli("eu-cdbr-azure-north-e.cloudapp.net","b007c3ffbd1a09", "e4d0b324", "rgutest_cmm503_HRLT");
+//if($mysqli->connect_errno) {
+  //  echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ")" . $mysqli->connect_error;
+//}{
+    //if(!$mysqli->query("DROP PROCEDURE IF EXISTS Login1") ||
+      //  !$mysqli->query("CREATE PROCEDURE Login1(IN id_val INT) BEGIN INSERT INTO test(id) VALUES(id_val); END;"))
         {
-        echo "Stored procedure creation failed:(".$mysqli->connect_errno.")".$mysqli->error;
+        //echo "Stored procedure creation failed:(".$mysqli->connect_errno.")".$mysqli->error;
         }
       //  }
 //prepared statements - possible use within coursework
-//if(!($stmt=$mysqli->prepare("INSERT INTO test(id) VALUES(?)"))) {
-  //  echo "Prepare Failed: (".$mysqli->connect_errno.")".$mysqli->error;
+if(!($stmt=$mysqli->prepare("INSERT INTO test(id) VALUES(?)"))) {
+    echo "Prepare Failed: (".$mysqli->connect_errno.")".$mysqli->error;
     /*binding the parameters*/
-    //$id=1;
-    //if (!$stmt->bind_param("i", $id)) {
-      //  echo "Binding parameters failed: (",$stmt->errno.")".$stmt->error;
-    //}
-    //if(!$stmt->execute()){
-      //  echo "Execute failed: (".$stmt->errno. ")".$stmt->error;
-   // }
+    $id=1;
+    if (!$stmt->bind_param("i", $id)) {
+        echo "Binding parameters failed: (",$stmt->errno.")".$stmt->error;
+    }
+    if(!$stmt->execute()){
+        echo "Execute failed: (".$stmt->errno. ")".$stmt->error;
+   }
 }
 ?>
