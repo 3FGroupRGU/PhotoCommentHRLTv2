1416d83abeefe52608a8857eda26dea631719899
HenryRL <1300427@rgu.ac.uk>
2016-04-24 13:23:44 +0100

    Full file security update 13:23 240416

--- login.php
+++ login.php
@@ -8,12 +8,24 @@
 		if(empty($_POST["username"]) || empty($_POST["password"]))
 		{
 			$error = "Both fields are required.";
-		}else
-		{
+		}else {
 			// Define $username and $password
-			$username=$_POST['username'];
-			$password=$_POST['password'];
-			
+			$username = $_POST['username'];
+			$password = $_POST['password'];
+		}
+			//validating information inserted in the above field matches what is expected in username field
+		{
+			if (isset($_POST['username']))
+				$username = stripslashes(trim($_POST['username']));
+				$username = strip_tags(trim($_POST['username']));
+		}
+			//validating information inserted in above field matches what is expected in password field
+		{
+			if(isset($_POST['password']))
+			$password = stripslashes(trim($_POST['password']));
+			$username = strip_tags(trim($_POST['password']));
+		}
+		
 			//Check username and password from database
 			$sql="SELECT userID FROM users WHERE username='$username' and password='$password'";
 			$result=mysqli_query($db,$sql);
@@ -21,8 +33,6 @@
 			
 			//If username and password exist in our database then create a session.
 			//Otherwise echo error.
-
-
 			
 			if(mysqli_num_rows($result) == 1)
 			{
