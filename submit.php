533b7df7d58df3c50628f5a9452c7187396ce76d
HenryRL <1300427@rgu.ac.uk>
2016-04-27 16:16:26 +0100

    Changes to submit.php. 16:16 270416

--- submit.php
+++ submit.php
@@ -10,7 +10,7 @@ if(isset($_POST["submit"]))
     $clean_password=mysqli_real_escape_string ($db, $password);
     $clean_email=mysqli_real_escape_string ($db, $email);
 
-    $sql="SELECT email FROM users WHERE email='$email'";
+    $sql="SELECT email FROM users WHERE email='$clean_email'";
     $result=mysqli_query($db,$sql);
     $row=mysqli_fetch_array($result,MYSQLI_ASSOC);
     if(mysqli_num_rows($result) == 1)
