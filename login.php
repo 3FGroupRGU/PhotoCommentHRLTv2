66448853a3a1e316a5a2392aeeb66681966a6c59
HenryRL <1300427@rgu.ac.uk>
2016-04-28 23:03:07 +0100

    Login Form re-do. Login attempt restrictions. 23:02 280416

--- login.php
+++ login.php
@@ -11,7 +11,10 @@
 			$error = "Both fields are required.";
 		}else
 		{
-			// Define $username and $password
+
+			//get users IP
+			$userIP=$_SERVER['REMOTE_ADDR'];
+					// Define $username and $password
 			$username=$_POST['username'];
 			$password=$_POST['password'];
 
@@ -37,5 +40,44 @@
 
 		}
 	}
+if (isset($_POST['submit_login'])) {
 
+	if (isset($_POST['username']) && isset($_POST['password'])) {
+		$username = mysqli_real_escape_string($_POST['username']);
+		$password = mysqli_real_escape_string($_POST['password']);
+		// id = unique primary key
+		$rs = mysqli_query('SELECT id,Username,Password,Failed_logins,IP_address FROM Users WHERE Username = '.$username.'');
+		$num = mysql_num_rows($rs);
+		if ($num > 0) {
+			$row = mysqli_fetch_array($rs);
+			if ($password == $row['Password']) {
+				mysqli_query('UPDATE Users SET Failed_logins = 0 WHERE id = '.$row['id'].'');
+				header('location: photos.php');
+			} else {
+				// Failed password check
+				if ($row['Failed_logins'] > 3) {
+
+				} else {
+					$ip = $_SERVER['REMOTE_ADDR'];
+					if ($row['IP_address'] != $ip) {
+						// New ip adress, reset failed logins
+						$failed_logins = 0;
+					} else {
+						// Increment failed logins
+						$failed_logins = $row['Failed_logins']+1;
+					}
+					mysqli_query('UPDATE Users SET Failed_logins = '.$failed_logins.',IP_address = '.$ip.' WHERE id = '.$row['id'].' ');
+				} // End check Failed_logins > 3
+			}
+		} else {
+			// No such Username found in database
+			$error = 'no_such_username';
+		} // End username check from database
+
+	} else {
+		// Either username or password is missing
+		$error = 'incomplete_form';
+	} // end check for username and password
+
+} // end main submit_login check
 ?>
